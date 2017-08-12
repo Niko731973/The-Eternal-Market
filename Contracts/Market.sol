2ee4730f7b96c95ea979337e8760311ba93d4f2c
Scott Grimes <scott-grimes@users.noreply.github.com>
2017-08-11 23:44:39 -0500

    Update Market.sol

--- Contracts/Market.sol
+++ Contracts/Market.sol
@@ -1,171 +1,271 @@
 pragma solidity ^0.4.2;
-import "Order.sol";
-import "Base.sol";
+import "./Purchase.sol";
 
 contract Market {
-
-	/* The Eternal Address of the Market */
-    address eternalAddress;
-
     
-    /*Contract Variables and Events*/
+/* Contract Variables */
        
-    uint public listing_rate = 5;          			// Listing rate is a fee charged for each new listing as a percent (ie 12% charge would be 12)
-    uint public order_rate = 2;                     // Order rate is a fee charged on each order submitted as a percent( ie 2% charge would be 2)
+    uint public listing_rate = 5;          			// Listing rate is the fee charged for each new listing as a percent (ie 12% charge would be 12)
+    uint public order_rate = 2;                     // Order rate is the fee charged on each order submitted as a percent( ie 2% charge would be 2)
     uint public bad_seller_threshold = 15;          //If a listing has more than this amount (as a percentage) of disputed sales, any user can remove this listing.
-    uint public nextFreeListingID = 1;		           //the next free listing ID availible
-    uint public nextFreeOrderID = 1;       //the next free order ID availible
-    mapping (uint => Listing) public listings;
-    mapping (uint => address) public orders;
+    uint public nextFreeListingID = 1;		        //the next free listing ID availible
+    uint public nextFreeOrderID = 1;       			//the next free order ID availible
+    mapping (uint => Listing) public listings;		// mapping of listings
+    mapping (uint => Order) public orders;			// mapping of orders
+    address public owner;							// the owner address of the market
 
-    /* Structures representing a listing and an order */
+    /* Listing Structure */
     struct Listing {
     
-        address seller;
-        string title;
-        string listingDescription;
+        address seller;						// ether address of the seller 
+        string title;						// listing title
+        string listingDescription;			// sellers description of the listing. may include links to images, other info.
         string publicKey;					//public key of the seller used to encrypt shipping addresses for orders
-        uint price;
-        uint timeListed;
-        bool enabled;
+        uint price;							//price in wei of the listing
+        uint timeListed;					// date and time the listing was created
+        bool enabled;						// is the listing active or not
+        uint successes;						// # of successful orders
+        uint aborted;						// # of aborted orders
+        uint disputed;						// # of disputed orders
     }
     
-    function Market(address _eternal){
-    eternalAddress = _eternal;
+    /* Order Structure */
+    
+    struct Order {
+    
+	    address seller;
+	    address buyer;
+	    uint listingID;
+	    string shippingDetails;			// the shipping address of the buyer, encrypted with the sellers public key
+	    uint timeTracker;   			// time the contract was created. if the order has been finished this shows the time feedback was issued instead
+	    uint state;         		    // 0 unconfirmed, 1 shipped, 2 successful, 3 disputed, 4 aborted, 5 deadman activated
+	    string feedback;    			// buyer-submitted feedback on the order
+	    uint stars;						// buyer-submitted 1 through 5 star rating system
+	    address contractAddress;        // the contract location which stores the ether
+	    
     }
     
-    /* Returns the current address of the community contract */
-    function getCommunity() constant returns (address){
-    	Base b = Base(eternalAddress);
-    	return b.community();
-    }
-
-    /* Used to find only listings with a valid index (not out of bounds or disabled) */
-    modifier onlyValidListings(uint id) { 
-        if(id<1 || id>= nextFreeListingID){throw;}
-	_;
-    }
-    
-    /* Used to find only orders with a valid index (not out of bounds) */
-    modifier onlyValidOrders(uint id){
-        if(id<1 || id>= nextFreeOrderID){throw;}
-        _;
-    }
-    
-    /* only the community can execute some functions (via a proposal which is voted on and executed) */
-    modifier onlyCommunity() {
-    	if (msg.sender!=getCommunity()){ throw;}
-		_;
+    /* Market Constructor /*
+    function Market(){
+    	owner = msg.sender;
     }
     
     
     /*Constant Functions*/
     
-    // Compute the fee paid by the buyer for each order that is submitted
+    /* Used to find only listings with a valid index (not out of bounds) */
+    modifier onlyValidListings(uint id) { 
+        require(id>0 && id < nextFreeListingID);
+		_;
+    }
+    
+    /* Used to find only orders with a valid index (not out of bounds) */
+    modifier onlyValidOrders(uint id){
+        require(id>0 && id < nextFreeOrderID);
+        _;
+    }
+    
+    /* Returns the fee charged for an order in wei */
     function buildOrderFee(uint price) public constant returns(uint){
-        //returns the fee charged for an order
         return (price*order_rate)/100;
     }
     
-    // Compute the fee charged to the seller when a new listing is created
+    /* Returns the fee charged for a new listing in wei*/
     function getListingFee(uint price) public constant returns(uint){
         return (price*listing_rate)/100;
     }
-    //returns successful, disputed, aborted listings
-    function getStats(uint _id) constant onlyValidListings(_id) returns(uint, uint, uint){
-    	address seller = listings[_id].seller;
-		
-		//figures out if a listing is bad
-		uint sales_disputed = 0;
-		uint sales_successful = 0;
-		uint sales_aborted = 0;
-		for(uint i = 1;i<nextFreeOrderID;i++){
-			if(orders[i]==seller){
-				uint state = Order(orders[i]).getState();
-				if(state == 2) {sales_successful++;}
-				else if(state == 3){sales_disputed++;}
-				else if(state == 4){sales_aborted++;}
-			}
-		}
-        
-    return(sales_successful, sales_disputed, sales_aborted);
-    }
     
-    // Determines if a listing is below the threshold to be considered for purging
+    /* Determines if a listing is below the threshold to be considered for purging */
     function isBadListing(uint _id) constant onlyValidListings(_id) returns(bool){
-		var (succ, disp,) = getStats(_id);
-		
-        uint listing_bad_rate = (disp*100)/(disp+succ);
+		Listing memory i = listings[_id];
+		if(i.disputed == 0 && i.successes == 0){return false;} 				//listing has no orders yet
+        uint listing_bad_rate = (i.disputed*100)/(i.disputed+i.successes);	//current dispute rate
         return listing_bad_rate>=bad_seller_threshold;
     }
     
-    /* Order and Listing functions*/
+    /* Seller and Buyer Functions */
     
-    //buyers place orders for a listing using this function
-    function addOrder (uint index, string shippingAddress) payable onlyValidListings(index) returns(bool){
-        Listing listing = listings[index];
+    /* Seller creates a new listing using this function */
+    function addListing (string _title, string _description, string _publicKey, uint _price) payable returns(bool){
         
-        //calculate the fee for the order
-        uint fee = buildOrderFee(listing.price);
+        //check to see that the ether provided is enough to pay the listing fee.
+        require(msg.value>=getListingFee(_price));
         
-        //you can only order if you paid enough money, and if the listing is enabled
-        if(msg.value < listing.price || !(listing.enabled)){ throw;}	
-
-        //create a purchase contract for the order. keep the fee, transfer the remainder to the contract
-        address orderAddress = (new Order).value(msg.value-fee)(msg.sender,listing.seller,index,shippingAddress,this);
-        orders[nextFreeOrderID] = orderAddress;
+        //add the new listing to our database
+        nextFreeListingID++;
+        listings[nextFreeListingID-1] = Listing(msg.sender,_title,_description,_publicKey,_price,now,true,0,0,0);
+        return true;
+    }
+    
+    /* Buyer places a new order with this function */
+    function addOrder (uint _id, string shippingAddress) payable onlyValidListings(_id) returns(bool){
+    
+        require(listings[_id].enabled);		//the listing for this order must be active
+        
+        //calculate the fee charged for the order
+        uint fee = buildOrderFee(listings[_id].price);
+        
+        //buyer can only order if they sent enough funds
+        require(msg.value >= listings[_id].price);	
+		
+        //create a purchase contract for the order. keep the fee, transfer the rest of the payment to the contract
+        address orderAddress = (new Purchase).value(msg.value-fee)(this);
+        
+        //add the order to the orders database
         nextFreeOrderID++;
+        uint i = nextFreeOrderID-1;
+        orders[i].contractAddress = orderAddress;
+        orders[i].buyer = msg.sender;
+        orders[i].seller = listings[_id].seller;
+        orders[i].listingID = _id;
+        orders[i].shippingDetails = shippingAddress;
+        orders[i].timeTracker = now;
+        
         return true;
         
      }
     
     
-    //a seller can create a new listing using this function
-    function addListing (string _title, string _description, string _publicKey, uint _price) payable returns(bool){
-        
-        //check to see that the ether provided is enough to pay the listing fee.
-        if(getListingFee(_price)>msg.value){ throw; }
-        
-        //add the new listing to our database
-        listings[nextFreeListingID] = Listing(msg.sender,_title,_description,_publicKey,_price,now,true);
-        nextFreeListingID++;
-    	
-        
-        return true;
-    }
-
-    //disables a listing
+    /* Used to remove a listing */
     function removeListing(uint index) onlyValidListings(index) returns (bool){
         
-        
-       //A listing can be removed in only three cases:
-       //1) The seller wishes to remove their own listing
-       //2) The listing has a high percent of disputed transactions 
-       //3) The shareholders vote for the listing to be removed
-       //This allows any user to purge a listing created by a bad actor
-       if (!((msg.sender == listings[index].seller) || isBadListing(index) || (msg.sender==getCommunity()))) {throw;}
+       // A listing can be removed in only two cases:
+       // 1) The seller wishes to remove their own listing
+       // 2) The listing has a high ratio of disputed to successful transactions
+       // This allows any user to purge a listing created by a bad actor
+       
+       require ((msg.sender == listings[index].seller) || isBadListing(index));
        
 	   listings[index].enabled = false;
 	   
        return true;
      }
      
-     /* a seller can change the price of their order at any time to compensate for changes in 
-     the value of ether. there is no fee to reduce the price of a listing. if a seller increases
-     the price of a listing, they must pay the listing fee only for the difference in cost between
-     the old and new price */
+     /* Seller can change the price of a listing */
      function changePrice(uint id, uint newPrice) payable onlyValidListings(id) returns (bool){
      
-     		if(msg.sender!= listings[id].seller){throw;}
-	    	if(!listings[id].enabled){throw;}
-     		if(newPrice>listings[id].price && msg.value<( getListingFee(newPrice-listings[id].price))){throw;}
+     	// a seller can change the price of their listing at any time to compensate for changes in 
+     	// the value of ether. there is no fee to reduce the price of a listing. if a seller increases
+     	// the price of a listing, they must pay the listing fee only for the difference in cost between
+     	// the old and new price 
+     
+     		require(msg.sender == listings[id].seller);
+	    	require(listings[id].enabled);
+	    	if(newPrice>listings[id].price){
+     			require( msg.value>=( getListingFee(newPrice-listings[id].price)));
+     		}
      		listings[id].price = newPrice;
-     	
+     		
+     		return true;
      }
     
-    //any funds in the market are transferred as profits to the shareholders
-    function getProfits() {
-        getCommunity().transfer(this.balance);
+    
+    
+    /* Functions used to execute transactions */
+    
+    /* Abort an unconfirmed order before it is shipped */
+    function abort(uint _id) onlyValidOrders(_id) returns(bool){
+    
+    /// The purchase can be aborted by the seller before they confirm shipment. 
+    /// The purchase can also be aborted by the buyer if the seller has not confirmed shipment within three days.
+    
+    	require(orders[_id].state == 0); 
+    	require( (msg.sender==orders[_id].seller) || (msg.sender==orders[_id].buyer && now> (orders[_id].timeTracker + (3 days))) );
+        orders[_id].state = 4;
+        
+    	sendOrderFunds(_id,orders[_id].buyer);		// Buyer recieves a refund
+    	return true;
+    }
+
+	
+	/* Seller confirms the shipment of an order */
+    function confirmShippment(uint _id) onlyValidOrders(_id) returns(bool) {
+			
+		require(msg.sender == orders[_id].seller);
+		require(orders[_id].state== 0 );
+		        
+        orders[_id].state = 1;
+        return true;
+    }
+        
+    /* Buyer confirms delivery of the item, seller is paid. */
+    function confirmDelivery(uint _id, string _feedback, uint _stars) onlyValidOrders(_id) returns(bool) {
+    	require(msg.sender == orders[_id].buyer);
+    	require(orders[_id].state == 1);
+    	require(_stars>0 && _stars<6); // rating must be between 1 and 5 stars
+    	
+        orders[_id].state = 2;
+        orders[_id].feedback = _feedback;
+        orders[_id].stars = _stars;
+        orders[_id].timeTracker = now;
+    	sendOrderFunds(_id,orders[_id].seller); //send funds from this order to the seller
+    	return true;
+    }
+    
+    /* Deadman's Switch */
+    function recoverFunds(uint _id) onlyValidOrders(_id)   returns(bool)  {	
+    
+    ///If for whatever reason the buyer is incapacitated after the order is shipped, 
+    /// any user can trigger release of the funds to the seller once 12 weeks have elapsed.
+    
+        require(now>(orders[_id].timeTracker+(12 weeks))); //DISABLE FOR TESTING
+    	require(orders[_id].state == 1 );
+    	orders[_id].state = 5;
+    	orders[_id].timeTracker = now;
+    	sendOrderFunds(_id,orders[_id].seller);
+    	return true;
+    }
+    
+    function dispute(uint _id, string _feedback, uint stars) onlyValidOrders(_id) returns(bool) {	
+    //only the buyer can dispute an order, they must wait at least three weeks after shipment before doing so.
+    //recovered funds are transfered to the market
+    
+    	require(msg.sender==orders[_id].buyer);
+    	require(orders[_id].state == 1);
+        require(now> (orders[_id].timeTracker+(3 weeks)));
+        require(stars>0 && stars<6);
+            orders[_id].state = 3;
+    		sendOrderFunds(_id,this);
+            orders[_id].feedback = _feedback;
+            orders[_id].stars = stars;
+            orders[_id].timeTracker = now;
+            return true;
+       
+    }
+    
+    //internal method, used to send money to the buyer or seller from each order's contract
+    function sendOrderFunds(uint id, address dest) private {
+    	//calls the sendFunds method of the Order contract for order *id, and sends the
+    	//contracts funds to *dest
+    	
+    	
+    	
     }
  
+    
+    
+    /* Owner Functions */
+    
+    function changeOrderRate(uint _new){
+    require(msg.sender==owner);
+    	order_rate = _new;
+    }
+    
+    function changeListingRate(uint _new){
+    require(msg.sender==owner);
+    	listing_rate = _new;
+    }
+    
+    //any funds in the market are transferred as profits to the owner
+    function collectProfits() {
+    	require(msg.sender==owner);
+        owner.transfer(this.balance);
+    }
+    
+    /* Transfers ownership of TEM */
+    function transferOwner(address _newOwner){
+    	require(msg.sender==owner);
+    	owner = _newOwner;
+    }
+    
 }
